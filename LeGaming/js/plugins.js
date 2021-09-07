1dfdef3c4938682d5c6be32e35c677ec34417615
Dimitri <dimitri.smakulski@live.fr>
2021-09-07 09:36:03 +0200

    [DSM] test mapping inside house

--- LeGaming/js/plugins.js
+++ LeGaming/js/plugins.js
@@ -3,5 +3,7 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}}
+{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"YED_Tiled","status":false,"description":"v1.20 Plugin supports Tiled Map Editor maps with some additional\nfeatures. https://archeia.itch.io/tiled-plugin-for-rpg-maker-mv","parameters":{"Z - Player":"3","Z - Below Player":"1","Z - Above Player":"5","Half-tile movement":"false","Priority Tiles Limit":"256","Map Level Variable":"0"}},
+{"name":"CardGameBattleSystem","status":true,"description":"","parameters":{}}
 ];
